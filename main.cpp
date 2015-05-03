96f0b2388f214c0e22bb6b5b64277a29deb4fde8
Mitchell McCann <tud46355@temple.edu>
2015-05-02 21:05:20 -0400

    Added more signals/slots as well as commented them on what they all are needed for

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include <QtQml>
 #include "playlist.h"
 #include "playlist_handler.h"
+#include "communication/communicationhandler.h"
 #include "dataobject.h"
 #include <QtCore>
 #include <thread>
@@ -73,9 +74,10 @@ int main(int argc, char *argv[])
     spotifyCreds.password = password.toString();
 
     playlist_handler *ph = new playlist_handler();
+    CommunicationHandler *comm = new CommunicationHandler("http://10.109.136.88:8900");
 
     QDir homePath = QDir::currentPath() + "/../Alluvial";
-    QFile example(homePath.absolutePath() + "/music/GiDeMo/Rain Song.mp3");
+    QFile example(homePath.absolutePath() + "/music/GiDeMo/Little Braver (Album Ver.).mp3");
     example.open(QIODevice::ReadOnly);
     QByteArray dat = example.readAll();
     ph->play(dat);
@@ -96,6 +98,9 @@ int main(int argc, char *argv[])
     QObject *repButton = root->findChild<QObject*>("repeatButton");
 
     QObject *metadata = root->findChild<QObject*>("songMeta");
+    QObject *songSearchResults = root->findChild<QObject*>("songListModel");
+
+    QObject *searchBar = root->findChild<QObject*>("searchBar");
     // Initializing the spotify stuff.  This particular is used for testing and SHOULD be changed later on.
     //spotifyThread_id = pthread_create(spotifyThread, NULL, initSpotifyFromMain, (void*)&spotifyCreds);
 
@@ -111,13 +116,39 @@ int main(int argc, char *argv[])
     QObject *playlistDropDown = root->findChild<QObject*>("dropdownPlaylistOptions");
     QObject *trackListings = root->findChild<QObject*>("trackListings");
 
+    // Play the next song
+    QObject::connect(ph, SIGNAL(requestSong(QString)),
+        comm, SLOT(sendMediaRequest(QString)));
+
+    // Play a song clicked from the search results
+    QObject::connect(songSearchResults, SIGNAL(requestSong(QVariant, QVariant, QVariant, QVariant, int, QVariant, int)),
+        ph, SLOT(playSingleSong(QVariant, QVariant, QVariant, QVariant, int, QVariant, int)));
+
+    // Begin playing the song when it gets it
+    QObject::connect(comm, SIGNAL(onMediaReceived(QByteArray)),
+        ph, SLOT(play(QByteArray)));
+
+    // When the search results are gotten from the communicator, display them
+    QObject::connect(comm, SIGNAL(onSearchReceived(QJsonObject)),
+        ph, SLOT(onSearchReceived(QJsonObject)));
+
+    // Display the search results
+    QObject::connect(ph, SIGNAL(displaySearchResults(QVariant,QVariant,QVariant,QVariant,QVariant,QVariant,QVariant,QVariant)),
+        songSearchResults, SLOT(displaySearchResults(QVariant,QVariant,QVariant,QVariant,QVariant,QVariant,QVariant,QVariant)));
+
+    // Display the active songs meta data in Item Detail View
     QObject::connect(ph, SIGNAL(displayData(QVariant)),
         metadata, SLOT(infoChanged(QVariant)));
+
+    // Display a small loading screen when songs are changed
+    QObject::connect(ph, SIGNAL(loadingSong()),
+        metadata, SLOT(loadingSong()));
 
     // Pause or play the song
     QObject::connect(playButton, SIGNAL(playClicked()),
         ph, SLOT(playOrPause()));
 
+    // When the active song is changed by clicking it in playlist panel, play the new song
     QObject::connect(trackListings, SIGNAL(activeSongChanged(int)),
         ph, SLOT(jumpToSong(int)));
 
@@ -126,15 +157,19 @@ int main(int argc, char *argv[])
         ph, SLOT(setVolume(int)));
     volSlider->setProperty("value", 50);
 
+    // Rewind when the rewind button is clicked
     QObject::connect(rwButton, SIGNAL(startRewind()),
         ph, SLOT(startRewind()));
 
+    // Fast forward when the fast forward button is clicked
     QObject::connect(ffButton, SIGNAL(startFastForward()),
         ph, SLOT(startFastForward()));
 
+    // When the rewind button is released, resume the song
     QObject::connect(rwButton, SIGNAL(endRewind()),
         ph, SLOT(resetPlaybackRate()));
 
+    // When the fast forward button is released, resume the song
     QObject::connect(ffButton, SIGNAL(endFastForward()),
         ph, SLOT(resetPlaybackRate()));
 
@@ -142,36 +177,65 @@ int main(int argc, char *argv[])
     QObject::connect(playbackSlider, SIGNAL(playbackPosChanged(int)),
         ph, SLOT(skipTo(int)));
 
+    // When the song ends, work on loading the next song
+    QObject::connect(playbackSlider, SIGNAL(songFinished()),
+        ph, SLOT(nextSong()));
+
     // Progress the music bar based off of the song position
     QObject::connect(ph, SIGNAL(positionChanged(QVariant)),
         playbackSlider, SLOT(positionChanged(QVariant)));
 
+    // When the songs' data loads and the duration changes, fix the labels displayed
     QObject::connect(ph, SIGNAL(durationChanged(QVariant)),
         playbackSlider, SLOT(durationChanged(QVariant)));
 
+    // Toggle the shuffle button when clicked
     QObject::connect(shufButton, SIGNAL(clicked()),
         ph, SLOT(shuffleSwitch()));
 
+    // Toggle the repeat button when clicked
     QObject::connect(repButton, SIGNAL(clicked()),
         ph, SLOT(repeatSwitch()));
 
+    // Jump to the previous song when clicked
     QObject::connect(leftSkip, SIGNAL(clicked()),
         ph, SLOT(previousSong()));
 
+    // Skip to the next song
     QObject::connect(rightSkip, SIGNAL(clicked()),
         ph, SLOT(nextSong()));
 
+    // Change what songs are active when the active playlist is changed
     QObject::connect(playlistDropDown, SIGNAL(activePlaylistChanged(int)),
         ph, SLOT(changeTrackListings(int)));
 
+    // When the active playlist is changed by the playlist handler, display the active playlist
+    QObject::connect(ph, SIGNAL(activePlaylistChanged(QVariant)),
+        playlistDropDown, SLOT(playlistChanged(QVariant)));
+
+    // Display all songs when the active playlist songs' are changed
     QObject::connect(ph, SIGNAL(setTrackListings(QVariant)),
         trackListings, SLOT ( changeListings(QVariant)));
 
+    // Display all the playlists in the drop down menu when a playlist is added or dropped
     QObject::connect(ph, SIGNAL(setPlaylistListings(QVariant)),
         playlistDropDown, SLOT ( changePlaylistListings(QVariant)));
 
+    // Change which song is highlighted when the active song changes
     QObject::connect(ph, SIGNAL(changeActiveSong(QVariant)),
         trackListings, SLOT (changeActiveSong(QVariant)));
+
+    // Delete a song from the active playlist when it is closed
+    QObject::connect(trackListings, SIGNAL(deleteSongFromPlaylist(int)),
+        ph, SLOT (dropSong(int)));
+
+    // When a song is added to the playlist from the search screen, add it to the active playlist in the handler
+    QObject::connect(songSearchResults, SIGNAL(addSongToPlaylist(QVariant, QVariant, QVariant, QVariant, int, QVariant, int)),
+        ph, SLOT (addSong(QVariant, QVariant, QVariant, QVariant, int, QVariant, int)));
+
+    // Submit a query to the communicator when a search is submitted
+    QObject::connect(searchBar, SIGNAL(searchQuery(QVariant)),
+        comm, SLOT(sendSearchRequest(QVariant)));
 
     playlist_item *newSong0 = new playlist_item("#0", "song 0", 5);
     playlist_item *newSong1 = new playlist_item("#1", "song 1", 5);
